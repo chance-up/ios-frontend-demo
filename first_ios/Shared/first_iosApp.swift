39917dbe39d9bc3f93de93efa03b8c1c101a3771
chance-up <ccs6473@naver.com>
2022-06-21 17:54:49 +0900

    first commit

---
+++ first_ios/Shared/first_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_iosApp.swift
+//  Shared
+//
+//  Created by Chansub on 2022/06/21.
+//
+
+import SwiftUI
+
+@main
+struct first_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

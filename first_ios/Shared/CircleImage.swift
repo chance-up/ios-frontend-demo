8bbe7484306e4d378128ab9d8277effd645b9b08
front-admin <front-admin@kt.com>
2022-06-23 10:11:25 +0900

    push test
    push test

---
+++ first_ios/Shared/CircleImage.swift
@@ -0,0 +1,27 @@
+//
+//  CircleImage.swift
+//  first_ios
+//
+//  Created by Chansub on 2022/06/22.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(
+                    .white, lineWidth:4
+                )
+            )
+            .shadow( radius:7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
--- first_ios/Shared/ContentView.swift
+++ first_ios/Shared/ContentView.swift
@@ -9,8 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack{
+            MapView()
+                .frame(height: 300)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                    .foregroundColor(.blue)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+                
+                Text("Chance-up")
+                    .font(.subheadline)
+                    .foregroundColor(.brown)
+            }
             .padding()
+        }
     }
 }
 
